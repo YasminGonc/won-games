2d96e7268ee83d1079a489accd548c5d45a06ce8
YasminGonc <yasmin.goncalves97@hotmail.com>
2023-02-12 20:58:57 -0300

    first commit

---
+++ api/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '4988f00623475b610b3ad444594cd528'),
+    },
+  },
+});
---
+++ api/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '6ef4b2a1-2981-4b9c-85a0-5f4d581fdc62'
+};
