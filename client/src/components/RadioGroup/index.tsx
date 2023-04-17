1f065e533460fe7b71421bae61277372463277d9
YasminGonc <yasmin.goncalves97@hotmail.com>
2023-04-16 21:08:53 -0300

    Add: new attribute

--- client/src/components/RadioGroup/index.tsx
+++ client/src/components/RadioGroup/index.tsx
@@ -3,11 +3,18 @@ import * as S from './styles'
 export type RadioGroupProps = {
   items: string[]
   labelColor?: 'white' | 'black'
+  defaultValue?: string
+  name?: string
 }
 
-export function RadioGroup({ items, labelColor = 'white' }: RadioGroupProps) {
+export function RadioGroup({
+  items,
+  labelColor = 'white',
+  defaultValue,
+  name
+}: RadioGroupProps) {
   return (
-    <S.RadioGroupRoot>
+    <S.RadioGroupRoot name={name} defaultValue={defaultValue}>
       {items.map((item) => {
         return (
           <S.ItemWrapper key={item}>
