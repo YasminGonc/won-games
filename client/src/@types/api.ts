ef654d630cd343b581ab3d5e9af444189e4ccedc
YasminGonc <yasmin.goncalves97@hotmail.com>
2023-05-10 21:31:23 -0300

    Add: api to game pages

--- client/src/@types/api.ts
+++ client/src/@types/api.ts
@@ -2,6 +2,16 @@ type Developers = {
   name: string
 }
 
+type Rating = 'FREE' | 'pegi3' | 'pegi7' | 'pegi12' | 'pegi16' | 'pegi18'
+
+type Platforms = {
+  name: string
+}
+
+type Categories = {
+  name: string
+}
+
 type Game = {
   name: string
   slug: string
@@ -14,4 +24,30 @@ type Game = {
 
 export type QueryGames = {
   games: Game[]
+}
+
+type QueryGameBySlug = {
+  name: string
+  short_description: string
+  description: string
+  price: number
+  rating?: Rating
+  realease_date?: Date
+  gallery: {
+    src: string
+    label: string
+  }
+  cover?: {
+    src: string
+  }
+  developers: Developers[]
+  publisher: {
+    name: string
+  }
+  categories: Categories[]
+  platforms: Platforms[]
+}
+
+export type QueryGamesBySlug = {
+  games: QueryGameBySlug[]
 }
