6bd50532d3713d5c4b71a5afea5df868b219a8b0
YasminGonc <yasmin.goncalves97@hotmail.com>
2023-06-19 21:51:36 -0300

    Add: apollo on client

--- client/src/templates/Games/index.tsx
+++ client/src/templates/Games/index.tsx
@@ -1,8 +1,15 @@
+import { useQuery } from '@apollo/client'
+import { QueryGames } from '@/@types/api'
+import { QUERY_GAMES } from '@/graphql/queries/games'
+
 import * as S from './styles'
+
 import { Base } from '../Base'
 import { GameCard, GameCardProps } from '../../components/GameCard'
 import { ItemProps, ExploreSidebar } from '../../components/ExploreSidebar'
 import { Grid } from '../../components/Grid'
+import { Loading } from '../../components/Loading'
+
 import { CaretDown } from 'phosphor-react'
 
 export type GamesTemplateProps = {
@@ -10,7 +17,11 @@ export type GamesTemplateProps = {
   filterItems: ItemProps[]
 }
 
-export function GamesTemplate({ games, filterItems }: GamesTemplateProps) {
+export function GamesTemplate({ filterItems }: GamesTemplateProps) {
+  const { data, loading } = useQuery<QueryGames>(QUERY_GAMES, {
+    variables: { limit: 15 }
+  })
+
   const handleFilter = () => {
     return
   }
@@ -23,18 +34,29 @@ export function GamesTemplate({ games, filterItems }: GamesTemplateProps) {
       <S.Main>
         <ExploreSidebar items={filterItems} onFilter={handleFilter} />
 
-        <section>
-          <Grid>
-            {games?.map((item) => (
-              <GameCard key={item.title} {...item} />
-            ))}
-          </Grid>
+        {loading ? (
+          <Loading />
+        ) : (
+          <section>
+            <Grid>
+              {data?.games.map((game) => (
+                <GameCard
+                  key={game.slug}
+                  title={game.name}
+                  slug={game.slug}
+                  developer={game.developers[0].name}
+                  img={`http://localhost:1337${game.cover?.url}`}
+                  price={game.price}
+                />
+              ))}
+            </Grid>
 
-          <S.ShowMore role="button" onClick={handleShowMore}>
-            <p>Show More</p>
-            <CaretDown size={35} />
-          </S.ShowMore>
-        </section>
+            <S.ShowMore role="button" onClick={handleShowMore}>
+              <p>Show More</p>
+              <CaretDown size={35} />
+            </S.ShowMore>
+          </section>
+        )}
       </S.Main>
     </Base>
   )
